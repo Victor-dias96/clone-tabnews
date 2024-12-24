ea7c5aefc9601a1313ef8e2371eb7f6f3650fecc
Victor-dias96 <diasv3882@gmail.com>
2024-12-24 13:54:57 +0000

    adicionar arquivo pages

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Oi! Se você está acessando isso, quero deixar um agradecimento especial por me apoiar nessa jornada. Agora, posso compartilhar essa mensagem em um pequeno espaço da internet. Obrigado de verdade amo vocês e feliz natal!</h1>
+}
+
+export default Home;
